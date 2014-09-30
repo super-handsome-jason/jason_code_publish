4c86caf4c13fc81098878b1e8b5a772afdc1a537
Super Handsome Jason <jason@auer.com.tw>
2014-09-30 12:21:55 +0800

    Finish ada2014 homework1 part 2...

--- ada2014/homework1/2/Box.cpp
+++ ada2014/homework1/2/Box.cpp
@@ -1,43 +1,55 @@
 #include <iostream>
 #include <cstdlib>
+#include <list>
 
 using namespace std;
 
-class Box
+struct Box
 {
     public:
         long long w,h;
     private:
 
 };
-  int main(int argc, char* argv[])
-  {
-    int number_of_cases = 0;
-    scanf ("%d", &number_of_cases);
-    for (int i = 0; i < number_of_cases; ++i)
-    {
-      int number_of_boxes;
-      scanf ("%d", &number_of_boxes);
-      Box bb[number_of_boxes];
-      
-      for (int j = 0; j < number_of_boxes; ++j)
-      {
-        bb[j] = new Box();
-        scanf ("%lld%lld", &bb[j].w, &bb[j].h);
-      }
-      
-      int number_of_pairs = 0;
-      
-      for (int j = 0; j < bb.length; ++j)
-      {
-      	for (int k = j-1; k < bb.length; ++k)
-      	{
-      	  if ((bb[j].w >= bb[k].w && bb[j].h >= bb[k].h) ||
-      	     (bb[k].w > bb[j].w && bb[k].h > bb[j].h))
-      	     ++number_of_pairs;
-      	}
-      }
-      
-      printf("%d", number_of_pairs);
-    }
-  }
+int main(int argc, char* argv[])
+{
+	list<int> answer_list;
+	int number_of_cases = 0;
+	scanf ("%d", &number_of_cases);
+	for (int i = 0; i < number_of_cases; ++i)
+	{
+		int number_of_boxes;
+		scanf ("%d", &number_of_boxes);
+		list<Box> bb;
+		//Box bb[number_of_boxes];
+
+		for (int j = 0; j < number_of_boxes; ++j)
+		{
+			Box b;
+			scanf ("%lld%lld", &b.w, &b.h);
+			bb.push_back(b);
+		}
+
+		int number_of_pairs = 0;
+		list<Box>::iterator j, k;
+		for (j = bb.begin(); j != bb.end(); ++j)
+		{
+			for (k = bb.begin(); k != bb.end(); ++k)
+			{
+				if (j == k)
+			  		continue;//not to compare the same box
+				if ((j->w <= k->w && j->h <= k->h) ||
+				 (j->w <= k->h && j->h <= k->w))
+					++number_of_pairs;
+			}
+		}
+		
+		answer_list.push_back(number_of_pairs);
+	}
+	
+	//print answer...
+	list<int>::iterator i;
+	for (i = answer_list.begin(); i != answer_list.end(); ++i)
+		cout << *i << "\n"; 
+	cout << endl;
+}
